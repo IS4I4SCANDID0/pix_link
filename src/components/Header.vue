6ddb69fce9c3998da88726834fa33c7cd2d12c69
Isaias Candido <isaiascandido92@outlook.com>
2025-11-03 13:23:59 -0300

    chore(deps): Instalação e configuração do Vue 3, Vite, Tailwind CSS e fontes customizadas

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  defineProps<{message: string}>();
+</script>
+
+
+
+<template>
+  <header class="bg-blue-600 text-white p-4 font-manrope text-center">
+    <h1 class="text-3xl font-bold">Meu Aplicativo Vue com Tailwind CSS</h1>
+  </header>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Padrão que cobre todos os ficheiros dentro de src/
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'manrope': ['Manrope', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
